5d3731876ba612f0e54a2aace185347e23e372c0
Lee Warner <lee.warner@ddiworld.com>
2020-05-20 19:14:41 -0400

    Working on problem 3, did some easy ones

---
+++ 1317_Convert_Integer_to_the_Sum_of_Two_No-Zero_Integers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1317_Convert_Integer_to_the_Sum_of_Two_No_Zero_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var answer = GetNoZeroIntegers(11);
+        }
+
+        public static int[] GetNoZeroIntegers(int n)
+        {
+            var firstNum = 1;
+            var secondNum = n - 1;
+
+            while (firstNum.ToString().Contains('0') || secondNum.ToString().Contains('0'))
+            {
+                firstNum++;
+                secondNum--;
+            }
+
+            return new[] {firstNum, secondNum};
+        }
+    }
+}
---
+++ 1342_Number_of_Steps_to_Reduce_a_Number_to_Zero/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1342_Number_of_Steps_to_Reduce_a_Number_to_Zero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public int NumberOfSteps(int num)
+        {
+            var stepCount = 0;
+            while (num > 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                    stepCount++;
+                }
+                else
+                {
+                    num -= 1;
+                    stepCount++;
+                }
+            }
+
+
+            return stepCount;
+        }
+    }
+}
